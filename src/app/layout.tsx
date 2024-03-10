075c8f3c8070cd34f30c08e27c8a1c0b0b271869
Kyle Barr <kylebarr54321@gmail.com>
2024-03-10 14:56:24 -0600

    add some basic page formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-slate-900 text-slate-100">{children}</body>
+      <body className="bg-slate-900 text-slate-100">
+        <div className="w-full h-12 bg-slate-950 flex">
+          <div className="w-[12in] shrink grow-0 mx-auto h-full flex justify-between">
+            <span className="h-full flex items-center px-6 text-lg font-semibold select-none">Dark's Blog</span>
+            <NavBar />
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
+  );
+}
+
+function NavBar() {
+  return (
+    <ul className="h-full flex flex-row cursor-pointer select-none">
+      <NavItem>Home</NavItem>
+      <NavItem>About</NavItem>
+    </ul>
+  );
+}
+
+function NavItem({ children }: { children: React.ReactNode }) {
+  return (
+    <li className="h-full px-6 hover:bg-slate-800 active:bg-slate-700 flex items-center">{children}</li>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>main</div>
+    <div>
+      <PostBody title="Welcome to my Blog!">
+        <span className="text-3xl">Recent Posts</span>
+        <div>post</div>
+      </PostBody>
+    </div>
+  );
+}
+
+function PostBody(
+  { children, title, subtitle, imageUrl = "/img/df-bg.jpg" }: 
+  { children: React.ReactNode, title: string, subtitle?: string, imageUrl?: string }
+) {
+  return (
+    <div>
+      <div className={`bg-black w-full h-72 relative flex flex-col items-center justify-center overflow-clip`}>
+        <img src={imageUrl} className="absolute blur-sm brightness-75" />
+        <span className="md:text-5xl md:font-bold text-3xl font-semibold drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">
+          {title}
+        </span>
+        {!subtitle ? null : <span className="text-xl text-gray-300 pt-6 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{subtitle}</span>}
+      </div>
+      <BodyWrapper>
+        {children}
+      </BodyWrapper>
+    </div>
+  );
+}
+
+function BodyWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="p-12 w-[12in] shrink grow-0 mx-auto">
+      {children}
+    </div>
   );
 }
