a59905f4095ba700dd0ba3344825c22883ce0a14
Kyle Barr <kylebarr54321@gmail.com>
2024-03-10 22:43:10 -0600

    testing longer titles and more css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,15 +24,15 @@ export default function Home() {
 function PostCard(
   { 
     imageUrl = "/img/df-bg.jpg", 
-    title = "Title", 
+    title = "This is a really long title to see how the word wrap works", 
     description = "Just a little bit of description text. This could also be the start of the blog post. I have no idea, I just need to fill this space." 
   }: 
   { imageUrl?: string, title?: string, description?: string }
 ) {
   return (
     <div className="bg-gray-800 h-48 rounded-lg p-6 relative flex flex-col gap-4 overflow-clip group hover:-translate-y-[2px] cursor-pointer transition-transform duration-75">
-      <img src={imageUrl} className="absolute top-0 left-0 blur-sm brightness-[0.4] group-hover:brightness-50" />
-      <span className="text-3xl group-hover:text-2xl duration-75 font-semibold z-10 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{title}</span>
+      <img src={imageUrl} className="absolute top-0 left-0 blur-sm brightness-[0.4] group-hover:brightness-[0.2]" />
+      <span className="text-3xl group-hover:text-2xl group-hover:truncate duration-75 font-semibold z-10 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{title}</span>
       <span className="z-10 opacity-0 group-hover:opacity-100 duration-75 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{description}</span>
     </div>
   );
