d8c409dc2b69600ebdfeb5334b405ffc8c47c0a8
Kyle Barr <kylebarr54321@gmail.com>
2024-03-10 22:38:26 -0600

    funny css things for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,37 @@ export default function Home() {
     <div>
       <PostBody title="Welcome to my Blog!">
         <span className="text-3xl">Recent Posts</span>
-        <div>post</div>
+        <div className="grid grid-cols-3 gap-4 mt-6">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </div>
       </PostBody>
+    </div>
+  );
+}
+
+function PostCard(
+  { 
+    imageUrl = "/img/df-bg.jpg", 
+    title = "Title", 
+    description = "Just a little bit of description text. This could also be the start of the blog post. I have no idea, I just need to fill this space." 
+  }: 
+  { imageUrl?: string, title?: string, description?: string }
+) {
+  return (
+    <div className="bg-gray-800 h-48 rounded-lg p-6 relative flex flex-col gap-4 overflow-clip group hover:-translate-y-[2px] cursor-pointer transition-transform duration-75">
+      <img src={imageUrl} className="absolute top-0 left-0 blur-sm brightness-[0.4] group-hover:brightness-50" />
+      <span className="text-3xl group-hover:text-2xl duration-75 font-semibold z-10 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{title}</span>
+      <span className="z-10 opacity-0 group-hover:opacity-100 duration-75 drop-shadow-[0_1px_4px_rgba(0,0,0,1)]">{description}</span>
     </div>
   );
 }
